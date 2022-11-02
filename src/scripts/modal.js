7ce780ad3fe114b98834cecce7c277b5a6cba608
tempet <tempetflamer@gmail.com>
2022-11-02 23:31:52 +0100

    add nav.js

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,12 +1,3 @@
-function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
